909b392313569105bb8032c5681874743e1c8e28
fremariab <fredamariabeech@gmail.com>
2024-04-08 01:15:20 +0000

    changes changes

--- js/viewrhc.js
+++ js/viewrhc.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
           result += "<td class='location'>" + element.location + "</td>";
           result += "</tr>";
         });
-        document.getElementById("table_data").innerHTML += result;
+        document.getElementById("display_rhc_data").innerHTML += result;
       }
     },
     error: (error) => {
