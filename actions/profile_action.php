ac3bb039c529f5ba961a208bd0498a9d17a749ea
fremariab <fredamariabeech@gmail.com>
2024-04-10 21:11:21 +0000

    Refactor profile_action.php to remove unnecessary echo statement and improve error handling

--- actions/profile_action.php
+++ actions/profile_action.php
@@ -3,8 +3,6 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-echo "USER ID IS:";
-echo $_SESSION['user_id'];
 header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Method: GET');
