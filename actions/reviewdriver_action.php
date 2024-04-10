9672d450157c8622e08e2edfb8de6e8d80a4b2ac
fremariab <fredamariabeech@gmail.com>
2024-04-10 21:10:13 +0000

    Refactor profile_action.php to use session_start() and display user ID

--- actions/reviewdriver_action.php
+++ actions/reviewdriver_action.php
@@ -1,8 +1,6 @@
 <?php
 /*reviewdriver_action.php */
 
-
-
 header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Method: GET');
--- view/userprofile.php
+++ view/userprofile.php
@@ -1,62 +1,61 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
+ <!DOCTYPE html>
+ <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <title>User Profile</title>
-    <link rel="stylesheet" href="../css/dashstyle.css">
-    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+ <head>
+     <meta charset="UTF-8">
+     <title>User Profile</title>
+     <link rel="stylesheet" href="../css/dashstyle.css">
+     <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 
-</head>
+ </head>
 
-<body>
-    <div class="wrapper">
-        <div class="sidebar">
-            <div class="logo">
-                <a href="../view/userdash.php"><img src="../images/logo.png"></a>
-            </div>
-            <ul>
-                <li><a href="../view/userdash.php"><i class="fas fa-home"></i>Home</a></li>
-                <li><a href="../view/userprofile.php"><i class="fas fa-user"></i>Profile</a></li>
-                <li><a href="../view/finddriver.php"><i class="fa-solid fa-magnifying-glass"></i> Find Driver</a></li>
-                <li><a href="../view/reviewdriver.php"><i class="fas fa-comments"></i></i>Review Driver</a></li>
+ <body>
+     <div class="wrapper">
+         <div class="sidebar">
+             <div class="logo">
+                 <a href="../view/userdash.php"><img src="../images/logo.png"></a>
+             </div>
+             <ul>
+                 <li><a href="../view/userdash.php"><i class="fas fa-home"></i>Home</a></li>
+                 <li><a href="../view/userprofile.php"><i class="fas fa-user"></i>Profile</a></li>
+                 <li><a href="../view/finddriver.php"><i class="fa-solid fa-magnifying-glass"></i> Find Driver</a></li>
+                 <li><a href="../view/reviewdriver.php"><i class="fas fa-comments"></i></i>Review Driver</a></li>
 
-                <li><a href="../view/reportinc.php"><i class="fas fa-address-card"></i>Report Incident</a></li>
-                <li><a href="../view/sayfforum.php"><i class="fa-solid fa-users"></i> Forum</a></li>
-                <li><a href="../login/logout.php"><i class="fa-solid fa-right-from-bracket" style="margin-top: 135px;"></i> Logout</a></li>
+                 <li><a href="../view/reportinc.php"><i class="fas fa-address-card"></i>Report Incident</a></li>
+                 <li><a href="../view/sayfforum.php"><i class="fa-solid fa-users"></i> Forum</a></li>
+                 <li><a href="../login/logout.php"><i class="fa-solid fa-right-from-bracket" style="margin-top: 135px;"></i> Logout</a></li>
 
-            </ul>
+             </ul>
 
-        </div>
-        <div class="main_content">
-            <div class="header">
-                <div class="headtext">User Profile</div>
-            </div>
-            <div class="info">
-                <div class="userprof">
-                    <div class="username" id="username">
-                        <h1 id="usernameh1">@ </h1>
-                        <span id="email"> </span>
-                    </div>
-                    <div class="profdescription">
-                        <p><i>Contact:</i></p>
+         </div>
+         <div class="main_content">
+             <div class="header">
+                 <div class="headtext">User Profile</div>
+             </div>
+             <div class="info">
+                 <div class="userprof">
+                     <div class="username" id="username">
+                         <h1 id="usernameh1">@ </h1>
+                         <span id="email"> </span>
+                     </div>
+                     <div class="profdescription">
+                         <p><i>Contact:</i></p>
 
-                        <p id="contact"> </p>
-                    </div>
-                    <ul class="profstats">
-                        <li><span id="numreviews"> </span>Reviews</li>
-                        <li><span id="numposts"> </span>Posts</li>
-                    </ul>
-                </div>
+                         <p id="contact"> </p>
+                     </div>
+                     <ul class="profstats">
+                         <li><span id="numreviews"> </span>Reviews</li>
+                         <li><span id="numposts"> </span>Posts</li>
+                     </ul>
+                 </div>
 
-            </div>
-        </div>
-    </div>
-    </div>
-    <script src="https://kit.fontawesome.com/88061bebc5.js" crossorigin="anonymous"></script>
-    <script src="../js/profile.js"></script>
-</body>
+             </div>
+         </div>
+     </div>
+     </div>
+     <script src="https://kit.fontawesome.com/88061bebc5.js" crossorigin="anonymous"></script>
+     <script src="../js/profile.js"></script>
+ </body>
 
-</html>
+ </html>
