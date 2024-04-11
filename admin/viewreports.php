49a51a40170f7da68ba5056e069b0a1089b29c36
fremariab <fredamariabeech@gmail.com>
2024-04-11 23:17:52 +0000

    Update margin-top value in logout links

--- admin/viewreports.php
+++ admin/viewreports.php
@@ -22,7 +22,7 @@
                 <li><a href="../admin/viewreports.php"><i class="fas fa-comments"></i></i>View Reports</a></li>
                 <li><a href="../admin/addrhc.php"><i class="fas fa-address-card"></i>Add RH Company</a></li>
                 <li><a href="../admin/sayfforum.php"><i class="fa-solid fa-users"></i> Forum</a></li>
-                <li><a href="../login/logout.php"><i class="fa-solid fa-right-from-bracket" style="margin-top: 40px;"></i> Logout</a></li>
+                <li><a href="../login/logout.php"><i class="fa-solid fa-right-from-bracket" style="margin-top: 15px;"></i> Logout</a></li>
             </ul>
 
         </div>
