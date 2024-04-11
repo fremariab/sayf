878c5b464c69ca5fcb8061275c47206787b337de
fremariab <fredamariabeech@gmail.com>
2024-04-11 13:41:17 +0000

    Refactor driverdetails.js to improve URL parameter extraction and add comments

--- js/driverdetails.js
+++ js/driverdetails.js
@@ -1,5 +1,13 @@
-/**profile.js */
+/**driverdetails.js */
 document.addEventListener("DOMContentLoaded", function () {
+   function getUrlParameter(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    var results = regex.exec(location.search);
+    return results === null
+      ? ""
+      : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
   var driverId = getUrlParameter("did");
 
   $.ajax({
