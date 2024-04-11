408189adaa41b3d4350906a24a76f2b4cd557812
fremariab <fredamariabeech@gmail.com>
2024-04-11 22:58:58 +0000

    Fix error reporting and update error messages in reportinc_action.php and viewdriver_action.php

--- actions/viewdriver_action.php
+++ actions/viewdriver_action.php
@@ -1,7 +1,6 @@
 <?php
 /*viewrhc_action.php */
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+
 header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Method: GET');
