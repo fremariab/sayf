e233a362a5a9a950a3baa0c99846db223b764ba2
fremariab <fredamariabeech@gmail.com>
2024-04-11 14:19:52 +0000

    Refactor driverdetail_action.php to fix formatting issue

--- actions/driverdetail_action.php
+++ actions/driverdetail_action.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Method: GET');
 header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers, Authorization, X-Request-With');
-
+ 
 include('../functions/functions.php');
 
 $inputData = json_decode(file_get_contents("php://input"));
