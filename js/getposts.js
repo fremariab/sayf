2e85e9f2046779291a31a7fc1bfad502f87a2a22
fremariab <fredamariabeech@gmail.com>
2024-04-12 21:46:37 +0000

    n

--- js/getposts.js
+++ js/getposts.js
@@ -8,29 +8,35 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log(data, status);
       if (data.status == 200) {
         response = data;
-
+        response = data;
         let result = "";
+        var reviewContainer = $("#reviews");
+        reviewContainer.empty();
+        let buttonres = "";
 
-        result += "<div class='card card2'>";
-        result += "<p class='close'></p>";
-        result += "<p class='desc' id='desc'>" + element.review_text + "</p>";
-        if (element.uid == response.user_id) {
-          result +=
-            "<div ><i id='deleteAction' onclick='confirmDelete(" +
-            element.revid +
-            "," +
-            element.did +
-            ") ; return false;' class='fa-solid fa-trash'></i></div>";
-        }
-        result += "</div>";
+        for (var index = 0; index < response.data.length; index++) {
+          var element = response.data[index];
 
-        if (
-          (index + 1) % resultsPerRow === 0 ||
-          index === response.data.length - 1
-        ) {
-          reviewContainer.append(result);
-          reviewContainer.append("</div>");
-          result = "";
+          result += "<div class='card card2'>";
+          result += "<p class='close'></p>";
+          result += "<p class='desc' id='desc'>" + element.review_text + "</p>";
+          if (element.uid == response.user_id) {
+            result +=
+              "<div ><i id='deleteAction' onclick='confirmDelete(" +
+              element.revid +
+              "," +
+              element.did +
+              ") ; return false;' class='fa-solid fa-trash'></i></div>";
+          }
+          result += "</div>";
+          if (
+            (index + 1) % resultsPerRow === 0 ||
+            index === response.data.length - 1
+          ) {
+            reviewContainer.append(result);
+            reviewContainer.append("</div>");
+            result = "";
+          }
         }
       }
     },
