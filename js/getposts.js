2ed9d5bfd4883090309ffdf24db7365cf0ddb3af
fremariab <fredamariabeech@gmail.com>
2024-04-12 21:53:47 +0000

    a

--- js/getposts.js
+++ js/getposts.js
@@ -8,26 +8,32 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log(data, status);
       if (data.status == 200) {
         response = data;
-        response = data;
         let result = "";
         var reviewContainer = $("#reviews");
         reviewContainer.empty();
+
+        // Define resultsPerRow or replace it with the appropriate value
+        var resultsPerRow = 2;
 
         for (var index = 0; index < response.data.length; index++) {
           var element = response.data[index];
 
           result += "<div class='card card2'>";
+          // Handle close button if needed
           result += "<p class='close'></p>";
           result += "<p class='desc' id='desc'>" + element.review_text + "</p>";
           if (element.uid == response.user_id) {
+            // Use onclick event to call confirmDelete function
             result +=
               "<div ><i id='deleteAction' onclick='confirmDelete(" +
               element.revid +
               "," +
               element.did +
-              ") ; return false;' class='fa-solid fa-trash'></i></div>";
+              "); return false;' class='fa-solid fa-trash'></i></div>";
           }
           result += "</div>";
+
+          // Append the result and close the row when needed
           if (
             (index + 1) % resultsPerRow === 0 ||
             index === response.data.length - 1
@@ -39,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       }
     },
+
     error: (error) => {
       var responseData = JSON.parse(error.responseText);
       document.getElementById("error").innerHTML = responseData.message;
