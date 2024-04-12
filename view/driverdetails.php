624e3a90eaafb157556e9cff66714fd2959c0107
fremariab <fredamariabeech@gmail.com>
2024-04-12 19:41:36 +0000

    Fix URL typo in deletereview.js and driverdetails.php

--- view/driverdetails.php
+++ view/driverdetails.php
@@ -100,56 +100,7 @@ $user_id = getUserID();
             });
         });
     </script>
-    <script>
-        function confirmDelete(revid, did) {
-            if (confirm("Are you sure you want to delete this assignment?")) {
-                window.location.href =
-                    "../actions/deletereview_action.php?revid=" + revid + "&did=" + did;
-                return false;
-            }
-        }
-
-
-        document.addEventListener("DOMContentLoaded", function() {
-            function getUrlParameter(name) {
-                name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-                var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-                var results = regex.exec(location.search);
-                return results === null ?
-                    "" :
-                    decodeURIComponent(results[1].replace(/\+/g, " "));
-            }
-
-
-
-            var revid = getUrlParameter("revid");
-            var did = getUrlParameter("did");
-            $("#submit").click(function(event) {
-                event.preventDefault();
-
-                $.ajax({
-                    url: "../actions/deletereview_action.php",
-                    method: "post",
-                    data: JSON.stringify({
-                        revid: revid,
-                    }),
-                    dataType: "json",
-                    success: (data, status) => {
-                        console.log(data, status);
-                        if (data.status == 201) {
-                            response = data;
-
-                            alert("Review deleted successfully");
-                        }
-                        window.location.href = "../view/driverdetails.php?did=" + did;
-                    },
-                    error: (error) => {
-                        $("#error").html(error.error);
-                    },
-                });
-            });
-        });
-    </script>
+    <script src="../js/deletereview.js"></script>
 </body>
 
 </html>
