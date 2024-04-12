9265be2fbf3aff6e5698d37d674b9de86e390de9
fremariab <fredamariabeech@gmail.com>
2024-04-12 04:33:27 +0000

    Update error handling and error message in finddriver.php

--- view/finddriver.php
+++ view/finddriver.php
@@ -67,8 +67,8 @@ $user_id = getUserID();
                 </div>
                 <div class="results" id="results" style="width:80%;display: flex;  margin-left:70px;  align-items: center;margin-top:90px;">
                     <center>
-                        <div class="error">
-                            
+                        <div class="error" id="error">
+
                         </div>
                         <h1>
                             Invest in <span class="spec" style="color:#E41D9E">
