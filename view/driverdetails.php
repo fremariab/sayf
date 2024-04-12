7ddc842733ce40b9ae4b0c3009964d1f3be6ae3a
fremariab <fredamariabeech@gmail.com>
2024-04-12 19:28:16 +0000

    Fix URL typo in deletereview.js and driverdetails.php

--- view/driverdetails.php
+++ view/driverdetails.php
@@ -100,8 +100,51 @@ $user_id = getUserID();
             });
         });
     </script>
-    <script src="../js/deletereview.js">
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            function getUrlParameter(name) {
+                name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+                var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+                var results = regex.exec(location.search);
+                return results === null ?
+                    "" :
+                    decodeURIComponent(results[1].replace(/\+/g, " "));
+            }
+            var revid = getUrlParameter("revid");
+            var did = getUrlParameter("did");
 
+            function confirmDelete(revid) {
+                if (confirm("Are you sure you want to delete this assignment?")) {
+                    window.location.href =
+                        "../actions/deletereview_action.php?revid=" + revid + "&did=" + did;
+                }
+            }
+
+            $("#submit").click(function(event) {
+                event.preventDefault();
+
+                $.ajax({
+                    url: "../actions/deletereview_action.php",
+                    method: "post",
+                    data: JSON.stringify({
+                        revid: revid,
+                    }),
+                    dataType: "json",
+                    success: (data, status) => {
+                        console.log(data, status);
+                        if (data.status == 201) {
+                            response = data;
+
+                            alert("Review deleted successfully");
+                        }
+                        window.location.href = "../view/driverdetails.php?did=" + did;
+                    },
+                    error: (error) => {
+                        $("#error").html(error.error);
+                    },
+                });
+            });
+        });
     </script>
 </body>
 
