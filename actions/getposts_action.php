07a372f7006f2b7b2e935bbb0cb4fe8f3d29cd2f
fremariab <fredamariabeech@gmail.com>
2024-04-12 21:48:54 +0000

    b

--- actions/getposts_action.php
+++ actions/getposts_action.php
@@ -8,15 +8,6 @@ header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,
 
 include('../functions/functions.php');
 
-$inputData = json_decode(file_get_contents("php://input"));
-
-$res;
-
-if (empty($inputData)) {
-    $res["msg"] = "No data received";
-    echo json_encode($res);
-    exit();
-}
 $getPosts = getPosts();
 
 echo $getPosts;
