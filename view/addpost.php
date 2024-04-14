4c1d06965afea480b0fe546b52fb9153ba398e35
fremariab <fredamariabeech@gmail.com>
2024-04-14 16:58:32 +0000

    Update userdash.php to add quick actions for Find Driver, Review Driver, Report Incident, Forum, and View Reports

--- view/addpost.php
+++ view/addpost.php
@@ -28,8 +28,8 @@ $user_id = getUserID();
                 <li><a href="../view/reviewdriver.php"><i class="fas fa-comments"></i></i>Review Driver</a></li>
                 <li><a href="../view/reportinc.php"><i class="fas fa-address-card"></i>Report Incident</a></li>
                 <li><a href="../view/viewreports.php"><i class="fas fa-eye"></i> View Reports</a></li>
-                <li><a href="../view/sayfforum.php"><i class="fa-solid fa-users"></i> Forum</a></li>
-                <li><a href="../login/logout.php"><i class="fa-solid fa-right-from-bracket" style="margin-top: 15px;"></i> Logout</a></li>
+                <?php if ($gender == 2 || $user_role == 1) { ?> <li><a href="../view/sayfforum.php"><i class="fa-solid fa-users"></i> Forum</a></li>
+                <?php } ?> <li><a href="../login/logout.php"><i class="fa-solid fa-right-from-bracket" style="margin-top: 15px;"></i> Logout</a></li>
             </ul>
         </div>
         <div class="main_content">
--- view/userdash.php
+++ view/userdash.php
@@ -3,7 +3,8 @@ include "../settings/core.php";
 ifLoggedIn();
 $user_role = getUserRole();
 $user_id = getUserID();
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -66,9 +67,8 @@ $user_id = getUserID();
         <li><a href="../view/finddriver.php"><i class="fa-solid fa-magnifying-glass"></i> Find Driver</a></li>
         <li><a href="../view/reviewdriver.php"><i class="fas fa-comments"></i> Review Driver</a></li>
         <li><a href="../view/reportinc.php"><i class="fas fa-address-card"></i> Report Incident</a></li>
-        <?php if()?>
-        <li><a href="../view/sayfforum.php"><i class="fa-solid fa-users"></i> Forum</a></li>
-        <li><a href="../view/viewreports.php"><i class="fas fa-eye"></i> View Reports</a></li>
+        <?php if ($gender == 2 || $user_role == 1) { ?> <li><a href="../view/sayfforum.php"><i class="fa-solid fa-users"></i> Forum</a></li>
+        <?php } ?> <li><a href="../view/viewreports.php"><i class="fas fa-eye"></i> View Reports</a></li>
         <li><a href="../login/logout.php"><i class="fa-solid fa-right-from-bracket" style="margin-top: 135px;"></i> Logout</a></li>
       </ul>
     </div>
