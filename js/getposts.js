b8485f4a9ec9adfc89cfbab0cc8aee8afd6d77a3
fremariab <fredamariabeech@gmail.com>
2024-04-14 15:45:04 +0000

    Refactor getposts.js to remove unnecessary closing div tag

--- js/getposts.js
+++ js/getposts.js
@@ -27,7 +27,6 @@ document.addEventListener("DOMContentLoaded", function () {
               element.posid +
               "); return false;' class='fa-solid fa-trash'></i></div>";
           }
-          result += "</div>";
 
           if (
             (index + 1) % resultsPerRow === 0 ||
